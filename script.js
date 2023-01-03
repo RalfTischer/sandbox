4b74c7f33dec719309e52281c7ce1c9b32cc156c
RalfTischer <108927724+RalfTischer@users.noreply.github.com>
2023-01-03 15:10:57 +0000

    parameter SUN added for size

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const SUN = 600; // canvas size as base for the other elements
+// const SUN = document.getElementById('canvas').width;
+const EARTH = 24 / 300 * SUN; 
+const EARTH_ORBIT = 105 / 300 * SUN; 
+const SHADOW_X = 40 / 300 * SUN;
+const SHADOW_Y = 36 / 300 * SUN;
+const MOON = 7 / 300 * SUN; 
+const MOON_ORBIT = 28.5 / 300 * SUN; 
+
 const sun = new Image();
 const moon = new Image();
 const earth = new Image();
@@ -15,12 +24,12 @@ function draw() {
   const ctx = document.getElementById("canvas").getContext("2d");
 
   ctx.globalCompositeOperation = "destination-over";
-  ctx.clearRect(0, 0, 300, 300); // clear canvas
+  ctx.clearRect(0, 0, SUN, SUN); // clear canvas
 
   ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
   ctx.strokeStyle = "rgba(0, 153, 255, 0.4)";
   ctx.save();
-  ctx.translate(150, 150);
+  ctx.translate(SUN / 2, SUN / 2);
 
   // Earth
   const time = new Date();
@@ -28,9 +37,9 @@ function draw() {
     ((2 * Math.PI) / 60) * time.getSeconds() +
       ((2 * Math.PI) / 60000) * time.getMilliseconds()
   );
-  ctx.translate(105, 0);
-  ctx.fillRect(0, -12, 40, 24); // Shadow
-  ctx.drawImage(earth, -12, -12, 24, 24);
+  ctx.translate(EARTH_ORBIT, 0);
+  ctx.fillRect(0, - SHADOW_Y / 3 , SHADOW_X, SHADOW_Y * 2 / 3); // Shadow
+  ctx.drawImage(earth, - EARTH / 2, - EARTH / 2, EARTH, EARTH);
 
   // Moon
   ctx.save();
@@ -38,17 +47,18 @@ function draw() {
     ((2 * Math.PI) / 6) * time.getSeconds() +
       ((2 * Math.PI) / 6000) * time.getMilliseconds()
   );
-  ctx.translate(0, 28.5);
-  ctx.drawImage(moon, -3.5, -3.5, 7, 7);
+  ctx.translate(0, MOON_ORBIT);
+  ctx.drawImage(moon, - MOON / 2, - MOON / 2, MOON, MOON);
   ctx.restore();
 
   ctx.restore();
 
   ctx.beginPath();
-  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+  ctx.arc(SUN / 2, SUN / 2, EARTH_ORBIT, 0, Math.PI * 2, false); // Earth orbit
   ctx.stroke();
 
-  ctx.drawImage(sun, 0, 0, 300, 300);
+  // Sub and background
+  ctx.drawImage(sun, 0, 0, SUN, SUN);
 
   window.requestAnimationFrame(draw);
 }
