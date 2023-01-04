7b145399e3171c851125d4ac4471682af1b27900
RalfTischer <108927724+RalfTischer@users.noreply.github.com>
2023-01-04 17:21:10 +0000

    fading Ln 36 out not properly working

--- flash.js
+++ flash.js
@@ -21,25 +21,30 @@ function flash() {
     
     const el = document.getElementById('canvas');
     const ctx = el.getContext('2d');
+    let color = 0;
     let dir, l ,x , y = 0;
 
     ctx.save();
     ctx.translate(el.clientWidth / 2, el.clientHeight / 2);     // center = 0,0
+
     dir = Math.random() * 2 * Math.PI;
     l = LENGTH * SIZE * (1 + (Math.random() - 0.5) * LENGTH_VAR);
 
     console.log(`Flash! dir = ${dir * 180 / Math.PI}°, l = ${l}, w = ${WIDTH * SIZE}, c = ${COLOR}`);
 
-    for (let i = 0; i < l; i++) {
-        ctx.fillStyle = COLOR;
-        ctx.strokeStyle = COLOR;
+    for (let i = 0; i < l; i+=1) {
+        color = `rgba(${main_playerColorOnly(2)}, ${(1 - (i / l)) })`   // fading out in time NOT WORKING PROPRLY
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
         ctx.lineWidth = WIDTH * SIZE;
         // ctx.beginPath(0,0);
         ctx.moveTo(0, 0);
         x = Math.sin(dir);
         y = Math.cos(dir);
-        ctx.lineTo(x, y);        
+        ctx.lineTo(x, y);              
         ctx.stroke();
+
+        // console.log(`c = ${color}`);
 
         ctx.translate(x, y);     // go to new position 
     }
